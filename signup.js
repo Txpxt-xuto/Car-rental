50b4f212bd3a267082c709b5609b978e2a6cf06f
Tapat toungsaakul <56795188+Txpxt-xuto@users.noreply.github.com>
2024-04-17 20:51:36 +0700

    Create signup.js

---
+++ signup.js
@@ -0,0 +1,16 @@
+const buttonsLike = document.querySelectorAll(".button_like")
+const buttonsLikeIcon = document.querySelectorAll(".button_like i")
+
+buttonsLike.forEach((item, index) => {
+    let buttonIcon = buttonsLikeIcon[index]
+
+    item.onclick = () => {
+        if(buttonIcon.classList.contains("far")) {
+            buttonIcon.classList.remove("far")
+            buttonIcon.classList.add("fas", "like_icon")
+        } else {
+            buttonIcon.classList.add("far")
+            buttonIcon.classList.remove("fas", "like_icon")
+        }
+    }
+})
