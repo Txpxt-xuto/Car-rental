a12bce5ff49b3886a7bcb5b87013b9eaa95b0a79
Tapat toungsaakul <56795188+Txpxt-xuto@users.noreply.github.com>
2024-04-19 14:43:07 +0700

    Create products-details.js

---
+++ products-details.js
@@ -0,0 +1,25 @@
+var productImg=document.getElementById("productImg");
+var smallImg=document.getElementsByClassName("small-img");
+            
+smallImg[0].onclick=function(){
+    productImg.src=smallImg[0].src;
+}
+smallImg[1].onclick=function(){
+    productImg.src=smallImg[1].src;
+}
+smallImg[2].onclick=function(){
+        productImg.src=smallImg[2].src;
+}
+smallImg[3].onclick=function(){
+        productImg.src=smallImg[3].src;
+}
+var menuItems=document.getElementById("MenuItems");
+MenuItems.style.maxHeight="0px";
+function menutoggle(){
+    if(MenuItems.style.maxHeight == "0px"){
+        MenuItems.style.maxHeight="200px";
+    }
+    else{
+        MenuItems.style.maxHeight="0px";
+    }
+}
