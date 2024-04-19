1905d2db715d314903bf02f4eecd6d9059858c97
Tapat toungsaakul <56795188+Txpxt-xuto@users.noreply.github.com>
2024-04-19 21:29:27 +0700

    Update home.js

--- home.js
+++ home.js
@@ -1,11 +1,18 @@
 var menuItems=document.getElementById("MenuItems");
             
-            MenuItems.style.maxHeight="0px";
-            function menutoggle(){
-                if(MenuItems.style.maxHeight == "0px"){
-                    MenuItems.style.maxHeight="200px";
-                }
-                else{
-                    MenuItems.style.maxHeight="0px";
-                }
-            }
+MenuItems.style.maxHeight="0px";
+function menutoggle(){
+if(MenuItems.style.maxHeight == "0px"){
+    MenuItems.style.maxHeight="200px";
+}
+else{
+    MenuItems.style.maxHeight="0px";
+}
+}
+const root = document.documentElement;
+const marqueeElementsDisplayed = getComputedStyle(root).getPropertyValue("--marquee-elements-displayed");
+const marqueeContent = document.querySelector("ul.marquee-content");
+root.style.setProperty("--marquee-elements", marqueeContent.children.length);
+for(let i=0; i<marqueeElementsDisplayed; i++) {
+    marqueeContent.appendChild(marqueeContent.children[i].cloneNode(true));
+}
