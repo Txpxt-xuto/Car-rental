a5a744dd881337e0b402f99f17324c0bf57bd06b
Tapat toungsaakul <56795188+Txpxt-xuto@users.noreply.github.com>
2024-04-29 20:38:07 +0700

    Update pythonemail.py

--- pythonemail.py
+++ pythonemail.py
@@ -2,7 +2,7 @@ import smtplib
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 
-from_addr='auto'
+from_addr='Sentfrom'
 to_addr=['Sendto@gmail.com']
 msg=MIMEMultipart()
 
