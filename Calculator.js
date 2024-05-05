c5ebd9d20b8bf68332f24ae36e2078760d007c77
Tapat toungsaakul <56795188+Txpxt-xuto@users.noreply.github.com>
2024-05-05 19:58:48 +0700

    Update Calculator.js

--- Calculator.js
+++ Calculator.js
@@ -8,8 +8,10 @@
     buttons.forEach()(function(button){
         button.addEventListener('click',function(e){
             let value = e.target.dataset.num;
-        }
-    }
+            screen.value += value;
+            
+        })
+    })
 
 
 
