f2c41dde522a2f726669d7f450a78c2f747bdfbe
Tapat toungsaakul <56795188+Txpxt-xuto@users.noreply.github.com>
2024-05-05 20:05:05 +0700

    Update Calculator.js

--- Calculator.js
+++ Calculator.js
@@ -9,10 +9,16 @@
         button.addEventListener('click',function(e){
             let value = e.target.dataset.num;
             screen.value += value;
-
         })
+    });
+
+    equal.addEventListener('click',function(e){
+        if(screen.value === ''){
+            screen.value = "";
+        }
+        else{
+            let answer = eval(screen.value);
+            screen.value = answer;
+        }
     })
-
-
-
 })();
