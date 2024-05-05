61c15df0267e273dd6b441adee657932a8df7a1a
Tapat toungsaakul <56795188+Txpxt-xuto@users.noreply.github.com>
2024-05-05 19:57:49 +0700

    Update Calculator.js

--- Calculator.js
+++ Calculator.js
@@ -1,1 +1,16 @@
-const
+(function(){
+
+    let screen = document.querySelector('.screen');
+    let buttons = document.querySelectorAll('.btn');
+    let equal = document.querySelector('.btn-green');
+    let clear = document.querySelector('.btn-red');
+
+    buttons.forEach()(function(button){
+        button.addEventListener('click',function(e){
+            let value = e.target.dataset.num;
+        }
+    }
+
+
+
+})();
