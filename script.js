d7605e248de7d278a65565348554a1eb017f8c44
Tapat toungsaakul <56795188+Txpxt-xuto@users.noreply.github.com>
2024-10-27 16:14:17 +0700

    Update script.js

--- script.js
+++ script.js
@@ -30,3 +30,33 @@ function toggleAccordion() {
 }
 
 items.forEach(item => item.addEventListener('click', toggleAccordion));
+
+
+
+
+
+/* Endless Scrolling Cards */
+let holder = document.querySelectorAll('.containerofslide')[0],
+  cards = document.querySelectorAll('.cardofslide');
+
+let preActiveCard = cards[1];
+let nextActiveCard = cards[2];
+
+function scrollLeft() {
+  holder.classList.remove('nextofslide');
+  holder.classList.remove('resetofslide');
+  holder.classList.add('nextofslide');
+  
+  preActiveCard.classList.remove('activeofslide');
+  nextActiveCard.classList.add('activeofslide');
+  setTimeout(reset, 600);
+}
+
+function reset() {
+  holder.classList.remove('nextofslide');
+  holder.classList.add('resetofslide');
+  preActiveCard.classList.add('activeofslide');
+  nextActiveCard.classList.remove('activeofslide');
+}
+
+setInterval(scrollLeft, 1500);
